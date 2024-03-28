4e02ef17f3639ab00c56d0533d0186ffd64e222f
rhtjddls123 <jjjk0605@naver.com>
2024-03-28 17:17:45 +0900

    Design: StartScreen 초기 설정
    shadow효과를 줄 때 android는 elevation으로 설정
    ios는 shadow로 시작하는 효과를 줘야함
    shadowColor: 그림자의 색상
    shadowOffset: 그림자의 위치
    shadowRadius: 그림자의 퍼짐 정도
    shadowOpacity: 그림자의 투명도

---
+++ MiniGame/screens/StartGameScreen.tsx
@@ -0,0 +1,26 @@
+import { TextInput, View, StyleSheet } from "react-native";
+
+const StartGameScreen = () => {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput />
+    </View>
+  );
+};
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: "#72063c",
+    elevation: 4,
+    borderRadius: 8,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});
