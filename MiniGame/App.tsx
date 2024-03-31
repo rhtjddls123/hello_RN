7eefd0f860cd371d2b16df99b2e6bd36f6a4e302
rhtjddls123 <jjjk0605@naver.com>
2024-03-31 17:03:29 +0900

    Design: 배경화면 사진 추가
    ImageBackground component를 사용하여 background image추가
    imageStyle property를 통해 opacity style 설정

--- MiniGame/App.tsx
+++ MiniGame/App.tsx
@@ -1,11 +1,17 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
     <LinearGradient colors={["#4e0429", "#ddb52f"]} style={styles.rootScreen}>
-      <StartGameScreen />
+      <ImageBackground
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+        source={require("./assets/images/background.png")}
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -13,5 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
